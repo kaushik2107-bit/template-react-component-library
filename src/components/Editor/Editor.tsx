495d58d1a5747dbb8f1fba4d0d5f278a252f3c82
Kaushik Bhowmick <kaushiksbhowmick@gmail.com>
2023-06-16 10:47:35 +0530

    first commit

---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface ButtonProps {
+    label: string;
+}
+
+export default function Editor(props: ButtonProps) {
+  return (
+    <button>{props.label}</button>
+  )
+}
